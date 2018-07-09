258c30314f1536970a8ed1929a0bf8382d84ab83
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2018-07-09 16:00:19 +0200

    Do not extend Collector

--- src/main/java/org/optaplanner/experiment/stream/api/collector/ConstraintStreamCollector.java
+++ src/main/java/org/optaplanner/experiment/stream/api/collector/ConstraintStreamCollector.java
@@ -3,24 +3,22 @@ package org.optaplanner.experiment.stream.api.collector;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 /**
- * Similar to {@link java.util.stream.Collector}, but with disperser (anti-accumulater) functionality,
+ * Similar to {@link java.util.stream.Collector}, but with disperser (anti-accumulater) functionality and
  * without combiner functionality.
  */
-public interface ConstraintStreamCollector<T, ResultContainer_, Result_> extends Collector<T, ResultContainer_, Result_> {
+public interface ConstraintStreamCollector<T, ResultContainer_, Result_> {
+
+    Supplier<ResultContainer_> supplier();
+
+    BiConsumer<ResultContainer_, T> accumulator();
 
     BiConsumer<ResultContainer_, T> disperser();
 
-    @Override
-    default BinaryOperator<ResultContainer_> combiner() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    default Set<Characteristics> characteristics() {
-        throw new UnsupportedOperationException();
-    }
+    Function<ResultContainer_, Result_> finisher();
 
 }
